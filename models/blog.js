9509f9401a9472e74d7648f82ac518472b62b826
themagiche <magiche.mc@gmail.com>
2019-08-22 14:22:51 +0300

    Blog initial manual

--- models/blog.js
+++ models/blog.js
@@ -2,15 +2,48 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const blogSchema = new Schema({
+const BlogSchema = new Schema({
+  author: {
+    type: String,
+    required: true
+  },
   title: {
     type: String,
     required: true
   },
-  description: {
+  content: {
     type: String,
     required: true
+  },
+  created_date: {
+    type: Date,
+    required: [true, "Created date is required"]
+  },
+  published_date: {
+    type: Date,
+    required: [true, "Created date is required"]
+  },
+  read_time: {
+    type: String,
+    required: false
+  },
+  thumbnail: {
+    type: String,
+    required: false
   }
 });
 
-module.exports = mongoose.model("blog", blogSchema);
+BlogSchema.methods.getBlogDetails = function() {
+  let blog = {
+    title: this.title,
+    author: this.author,
+    content: this.content,
+    created_date: this.created_date,
+    published_date: this.published_date,
+    read_time: this.read_time,
+    thumbnail: this.thumbnail
+  };
+  return blog;
+};
+
+module.exports = mongoose.model("blog", BlogSchema);
